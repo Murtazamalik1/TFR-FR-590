ff1f5bb5e3e2bb5f5966208144082472e7947550
murtaza <murtazamalik1319@gmail.com>
2022-09-20 19:38:48 +0530

    new branch

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    if(n<2):
-        return 1
-    return (fibonacci(n-1)+fibonacci(n-2))
-
-n=int(input("enter the number of term :"))
-for i in range(n):
-    print("fibonacci(",i,") = ",fibonacci(i))
-    
